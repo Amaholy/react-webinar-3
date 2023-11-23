13e5941884d6da6c2e90bef726779d80aa4eeadb
Amaholy <soundholy69@gmail.com>
2023-11-24 00:16:50 +0600

    Solution

--- src/store.js
+++ src/store.js
@@ -3,8 +3,14 @@
  */
 class Store {
   constructor(initState = {}) {
-    this.state = initState;
-    this.listeners = []; // Слушатели изменений состояния
+    this.state = {
+      list: [],
+    }
+    this.state.list = initState.list.map((item) => ({
+      ...item,
+      selectionCount: 0,
+    }))
+    this.listeners = [] // Слушатели изменений состояния
   }
 
   /**
@@ -13,10 +19,10 @@ class Store {
    * @returns {Function} Функция отписки
    */
   subscribe(listener) {
-    this.listeners.push(listener);
+    this.listeners.push(listener)
     // Возвращается функция для удаления добавленного слушателя
     return () => {
-      this.listeners = this.listeners.filter(item => item !== listener);
+      this.listeners = this.listeners.filter((item) => item !== listener)
     }
   }
 
@@ -25,7 +31,7 @@ class Store {
    * @returns {Object}
    */
   getState() {
-    return this.state;
+    return this.state
   }
 
   /**
@@ -33,20 +39,34 @@ class Store {
    * @param newState {Object}
    */
   setState(newState) {
-    this.state = newState;
+    this.state = newState
     // Вызываем всех слушателей
-    for (const listener of this.listeners) listener();
+    for (const listener of this.listeners) listener()
   }
 
   /**
    * Добавление новой записи
    */
   addItem() {
+    const newItem = {
+      code: this.generateUniqueCode(),
+      title: 'Новая запись',
+    }
     this.setState({
       ...this.state,
-      list: [...this.state.list, {code: this.state.list.length + 1, title: 'Новая запись'}]
+      list: [...this.state.list, newItem],
     })
-  };
+  }
+
+  /**
+   * Генерация уникального кода
+   * @returns {number}
+   */
+
+  generateUniqueCode() {
+    this.codeCounter = (this.codeCounter || 0) + 1
+    return this.codeCounter
+  }
 
   /**
    * Удаление записи по коду
@@ -55,9 +75,9 @@ class Store {
   deleteItem(code) {
     this.setState({
       ...this.state,
-      list: this.state.list.filter(item => item.code !== code)
+      list: this.state.list.filter((item) => item.code !== code),
     })
-  };
+  }
 
   /**
    * Выделение записи по коду
@@ -66,14 +86,18 @@ class Store {
   selectItem(code) {
     this.setState({
       ...this.state,
-      list: this.state.list.map(item => {
+      list: this.state.list.map((item) => {
         if (item.code === code) {
-          item.selected = !item.selected;
+          item.selected = !item.selected
+          item.selectionCount += item.selected ? 1 : 0
+        } else {
+          // Удаление выделение записи
+          item.selected = false
         }
-        return item;
-      })
+        return item
+      }),
     })
   }
 }
 
-export default Store;
+export default Store
